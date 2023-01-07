82328444e148aa599a553420d1b507e1d945da85
monad <syxddd@proton.me>
2023-01-06 19:52:32 -0500

    added make string

--- idea/makeString/src/Main.java
+++ idea/makeString/src/Main.java
@@ -9,22 +9,29 @@ public class Main {
         System.out.println("Enter Length of string: ");
         int lengthString;
         lengthString = inp.nextInt();
-        System.out.println("your string: " + createString(lengthString));
 
+        mainmenu(createString(lengthString));
     }
 
     public static String createString(int s)
     {
         Scanner inp = new Scanner(System.in);
-        String x;
+        String p;
         String newS = "";
         for (int i = 0; i<s; i++)
         {
-            x = inp.nextLine();
-            newS+=x;
+            p = inp.nextLine();
+            newS+=p;
         }
         return newS;
     }
 
+    public static void mainmenu(String s)
+    {
+        String a = s;
+        System.out.println(a);
+
+
+    }
 
 }
