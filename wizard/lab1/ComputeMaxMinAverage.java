9358af9de307c79d75464d01f78d1bc9e4598170
monad <syxddd@proton.me>
2023-01-26 16:08:15 -0500

    did stuff

---
+++ wizard/lab1/ComputeMaxMinAverage.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+/*
+ * This program reads double values from a list (actually from a string) using a scanner. It calculates the average of these values as
+ * well as the minimum value and the maximum value
+ * For example, if the values are: 20 10 30 40 then the average is (20+10+30+40)/4 = 25 and the min is 10 and the max is 40
+ */
+public class ComputeMaxMinAverage
+{
+	public static void main(String[] args)
+	{
+		String numbers = "34 68 22 76.0 81 98 78 84 62"; 
+		Scanner in = new Scanner(numbers);
+
+		double min = 10000000;
+		double max = 0;
+		double sum = 0.0;
+
+		// Use this variable to keep track of the number of floating point numbers read in from numbers string above
+		// After the loop is finished, count will be used when computing the average.
+		int count = 0;
+		//-----------Start below here. To do: approximate lines of code = 8
+		// Use a while loop and read one number at a time using the scanner in (see above). 
+		// Hint: use in.hasNextDouble() as the loop condition
+		// Inside the loop, update variable sum and update variables min and max if necessary
+		// Don't forget to increment variable count. 
+		// Hint: never call in.nextDouble() more than once inside a loop - call it once and assign the result to a variable. 
+		// Use this variable to test the value
+		
+		while (in.hasNextDouble())
+		{
+			double temp = in.nextDouble();
+			// 0  [1,2,3,4,5,6] 10000
+			if (temp < min)
+			{
+				 min = temp;
+				 sum += temp + max;
+			}
+
+			else if (temp > max)
+			{
+				max = temp;
+				sum += temp + min;
+				
+			}
+
+			
+			count ++;
+
+
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//-----------------End here. Please do not remove this comment. Reminder: no changes outside the todo regions.
+		System.out.printf("The average of the values is: %.2f%n", sum/count);
+		System.out.printf("The smallest value is %.2f%n", min);
+		System.out.printf("The largest value is %.2f%n", max);
+		System.out.printf("The range is %.2f%n", max - min);
+		System.out.println("Expected: \nThe average of the values is: 67.00\r\nThe smallest value is 22.00\r\nThe largest value is 98.00\r\nThe range is 76.00");
+	}
+}
---
+++ wizard/lab1/Scrabble.java
@@ -0,0 +1,169 @@
+import java.util.Scanner;
+
+// This program uses a static method letterValue() to determine the numeric value of a letter in Scrabble
+
+// Scrabble letter values:
+// A,E,I,O,U,L,N,S,T,R  = 1
+// D,G = 2
+// B,C,M,P = 3
+// F, H, V, W, Y = 4
+// K = 5
+// J, X = 8
+// Q, Z = 10
+
+public class Scrabble
+{
+	public static void main(String[] args)
+	{
+		String words = "abdicate kite quick zap about cattle";
+		Scanner in = new Scanner(words);
+		while (in.hasNext())
+		{
+			String word = in.next();
+			System.out.println("The Scrabble value of " + word + " is " + wordValue(word));
+		}
+		System.out.println("Expected: ");
+		System.out.println("The Scrabble value of abdicate is 13\nThe Scrabble value of kite is 8\nThe Scrabble value of quick is 20\nThe Scrabble value of zap is 14\nThe Scrabble value of about is 7\nThe Scrabble value of cattle is 8");
+
+	}
+	
+	/**
+      Finds the point value of a letter in Scrabble
+      @param ch the character to be evaluated
+      @return the letter point value (int)
+	 */
+	public static int letterValue(char ch)
+	{
+		int value = 0;
+		//-----------Start below here. To do: approximate lines of code = 23
+		// Use a switch statement (i.e. switch on the ch variable) to determine which letter is input and return that value of that letter. 
+		// See the comments at the top of the file for Scrabble letter values. 
+		// Convert ch to an uppercase letter first before using it in the switch statement
+		// Hint: the Character class has a static method called toUpperCase(). For example: ch = Character.toUpperCase(ch);
+		ch = Character.toUpperCase(ch);
+		switch (ch)
+		{
+			case 'A':
+				value = 1;
+				break;
+			case 'E':
+				value = 1;
+				break;
+			case 'I':
+				value = 1;
+				break;
+			case 'O':
+				value = 1;
+				break;
+			case 'U':
+				value = 1;
+				break;
+			case 'L':
+				value = 1;
+				break;
+			case 'N':
+				value = 1;
+				break;
+			case 'S':
+				value = 1;
+				break;
+			case 'T':
+				value = 1;
+				break;
+			case 'R':
+				value = 1;
+				break;
+			case 'D':
+				value = 2;
+				break;
+			case 'G':
+				value = 2;
+				break;
+			case 'B':
+				value = 3;
+				break;
+			case 'C':
+				value = 3;
+				break;
+			case 'M':
+				value = 3;
+				break;
+			case 'P':
+				value = 3;
+				break;
+			case 'F':
+				value = 4;
+				break;
+			case 'H':
+				value = 4;
+				break;
+			case 'V':
+				value = 4;
+				break;
+			case 'W':
+				value = 4;
+				break;
+			case 'Y':
+				value = 4;
+				break;
+			case 'K':
+				value = 5;
+				break;
+			case 'J':
+				value = 8;
+				break;
+			case 'X':
+				value = 8;
+				break;
+			case 'Q':
+				value = 10;
+				break;
+			case 'Z':
+				value = 10;
+				break;
+			default:
+				return value;
+
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//-----------------End here. Please do not remove this comment. Reminder: no changes outside the todo regions.
+		return value;
+	}
+
+	/**
+      Finds the point value of a word in Scrabble
+      @param str the word to be evaluated
+      @return the letter point value of the word 
+	 */
+	public static int wordValue(String str)
+	{
+		int total = 0;
+		for (int i = 0; i < str.length(); ++i)
+		{
+			total = total + letterValue(str.charAt(i));
+		}
+		return total;
+	}
+}
